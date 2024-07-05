e097d39d9d12a6af87a85e6e77369647f645418f
nicolas-lamb <nikelamb@gmail.com>
2024-07-05 16:20:05 -0300

    exemplos de exceções e polimorfismo

---
+++ AULA22/src/exemplos/exemploFinal/Aluno.java
@@ -0,0 +1,7 @@
+package exemplos.exemploFinal;
+
+//como a classe pessoa é final ela não pode ter subclasses
+//public class Aluno extends Pessoa{
+public class Aluno {
+
+}
---
+++ AULA22/src/exemplos/exemploFinal/Calculadora.java
@@ -0,0 +1,10 @@
+package exemplos.exemploFinal;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Operacoes op = new Operacoes();
+        //Não posso alterar o valor
+         //op.valor = 100;
+        System.out.println(Operacoes.PI);
+    }
+}
