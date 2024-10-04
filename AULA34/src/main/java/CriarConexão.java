aee7da1d8c5608f8d728c938984acc7ccf664c45
nicolas-lamb <nikelamb@gmail.com>
2024-10-04 11:33:01 -0300

    começando banco de dados

---
+++ AULA34/src/main/java/CriarConexão.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CriarConexão {
+    public static void main(String[] args) {
+        try{
+            //loader do driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //tentar conectar
+            Connection conexao = DriverManager.getConnection("jdbc:mysql"+"://localhost:3306/prog2sexta", "prog2sexta", "123456");
+            System.out.println("Estou conectado");
+            conexao.close();
+        }catch (ClassNotFoundException e){
+            System.out.println("Erro ao carrgar o driver!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
